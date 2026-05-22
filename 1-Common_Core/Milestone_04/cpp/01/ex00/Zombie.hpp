a605d825da6353bc8cca1039144d3d727b5c3cf3
Leonardo Aguiar De La Rocque <leoaguia@c2r2s2.42porto.com>
2026-05-22 23:16:23 +0100

    Cpp01: All files separates as designated on subject

--- 1-Common_Core/Milestone_04/cpp/01/ex00/Zombie.hpp
+++ 1-Common_Core/Milestone_04/cpp/01/ex00/Zombie.hpp
@@ -6,22 +6,31 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 14:54:36 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/05/22 20:01:36 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/05/22 21:35:45 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Zombie.hpp
 
-#include <iostream>
-#include <iomanip>
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+# include <iomanip>
 
 class Zombie
 {
 	private:
 		std::string	_name;
-		
+
 	public:
 		Zombie(std::string name);
 		void	announce(void);
 		~Zombie();
 };
+
+Zombie* newZombie(std::string name);
+void 	randomChump(std::string name);
+
+#endif
