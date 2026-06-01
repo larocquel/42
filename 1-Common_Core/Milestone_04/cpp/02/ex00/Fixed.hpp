e103eacbc3245c586e865e76e603ae779fdde0e6
larocquel <leonardolarocque@gmail.com>
2026-06-01 13:37:35 +0100

    Cpp 02: First Ortodox Canonical Form Class

---
+++ 1-Common_Core/Milestone_04/cpp/02/ex00/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/31 18:12:26 by leoaguia          #+#    #+#             */
+/*   Updated: 2026/06/01 13:36:04 by leoaguia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostring>
+
+class	Fixed
+{
+	private:
+		int					_value;	// Valor do número de ponto fixo
+		static const int	_fbits = 8;	// Número de bits fracionários, sempre = 8 (nesse exercício)
+
+	public:
+		// Forma Canônina Ortodoxa
+		Fixed(); // Construtor
+		Fixed(const Fixed& other);	// Construtor de cópia
+		Fixed&	operator=(const Fixed& other);	// Operador de cópia
+		~Fixed();	// Destrutor
+
+		// Getter e Setter
+		int getRawBits( void ) const; // returns the raw value of the fixed-point value.
+		void setRawBits( int const raw ); // that sets the raw value of the fixed-point number.
+};
+
+#endif
