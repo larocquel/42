8ec36a1d0b1e7158c745c04407454a950e4f344d
larocquel <leonardolarocque@gmail.com>
2026-06-01 21:46:17 +0100

    Cpp 02: ex01 Class

--- 1-Common_Core/Milestone_04/cpp/02/ex01/Fixed.cpp
+++ 1-Common_Core/Milestone_04/cpp/02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 18:38:33 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/01 19:54:11 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/06/01 21:24:05 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,14 @@ Fixed::Fixed(const float n)
 	std::cout << "Float constructor called" << std::endl;
 	float	tmp = n * (1 << _fBits);
 	this->_value = roundf(tmp);
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->_value >> _fBits);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)this->_value / (1 << _fBits));
 }
--- 1-Common_Core/Milestone_04/cpp/02/ex01/Fixed.hpp
+++ 1-Common_Core/Milestone_04/cpp/02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 18:38:14 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/01 19:53:49 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/06/01 21:39:49 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,32 @@
 # define FIXED_HPP
 
 # include <iostream>
-# include <cmath>	// roundf()
+# include <cmath> // roundf()
 
-class	Fixed
+class Fixed
 {
 	private:
-		int	_value;
+		int					_value;
 		static const int	_fBits = 8;
 
 	public:
-		Fixed(const int n);
-		Fixed(const float n);	// Como posso ter dois construtores e fazer a ortodox canonical form?
+		// Ortodox Canonical Form
+		Fixed();								// Construtor
+		Fixed(const Fixed& other);				// Copy Construtor de cópia
+		Fixed& operator=(const Fixed& other);	// Copy Assignment Operator
+		~Fixed();								// Destructor
 
+		// New Constructors
+		Fixed(const int n);						// Int Constructor
+		Fixed(const float n);					// Float Constructor
+
+		// Getter e Setter
+		int     getRawBits(void) const;
+		void    setRawBits(int const raw);
+
+		// Converters
+		int     toInt(void) const;				// Converts to int
+		float   toFloat(void) const;			// Converts to float
 };
 
 #endif
