d2cdb1bf9d586ad2da84910f22f8966f6b3fcdc6
larocquel <leonardolarocque@gmail.com>
2026-06-04 14:12:33 +0100

    Cpp 03: .hpp and Makefile

--- 1-Common_Core/Milestone_04/cpp/03/ex00/ClapTrap.hpp
+++ 1-Common_Core/Milestone_04/cpp/03/ex00/ClapTrap.hpp
@@ -6,23 +6,35 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 13:43:24 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/04 13:46:34 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/06/04 14:12:11 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
+# include <iosteram>
+# include <string>
+
 class ClapTrap
 {
 	private:
-
+			std::string	_name;
+			int			_hitPoints;
+			int			_energyPoints;
+			int			_attackDamage;
 	public:
+
 	// Ortodox Canonical Form
-	ClapTrap();										// Constructor
+	ClapTrap(std::string Name);						// Constructor
 	ClapTrap(const ClapTrap& other);				// Copy Contructor
 	ClapTrap&	operator=(const ClapTrap& other);	// Copy Assignment Operator
 	~ClapTrap();
+
+	// Mandatory Methods
+	void	attack(const std::string& target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
 };
 
 #endif
