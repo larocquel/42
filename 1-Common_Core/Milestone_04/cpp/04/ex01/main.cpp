ad9dcb0e92942add3bb368e6b11ea55b1c8392c1
larocquel <leonardolarocque@gmail.com>
2026-06-08 01:03:49 +0100

    Cpp 04: ex01 main and makefile fixed

--- 1-Common_Core/Milestone_04/cpp/04/ex01/main.cpp
+++ 1-Common_Core/Milestone_04/cpp/04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: leoaguia <leoaguia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 23:09:37 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/08 00:40:47 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/06/08 00:57:27 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,33 @@ int	main()
 		delete animals[i];
 		std::cout << std::endl;
 	}
+
+	std::cout << "----5. DEEP COPY TEST----" << std::endl;
+	Animal*	original = new Dog();
+	std::cout << std::endl;
+	Animal* copy;
+
+	copy = original;
+
+	// original
+	std::cout << "original - " << original->getType() << " sound: " ;
+	original->makeSound();
+
+	// copy
+	std::cout << "copy     - " << copy->getType() << " sound: " ;
+	copy->makeSound();
+
+	delete copy;
+
+	// is original alive?
+	std::cout << "original - " << original->getType() << " sound: " ;
+	original->makeSound();
+
+	// is copy alive?
+	std::cout << "copy     - " << copy->getType() << " sound: " ;
+	copy->makeSound();
+
+	delete original;
 
 	return (0);
 }
