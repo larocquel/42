b12ea2668df956b666b7b1c17ee493b24a2ebb3b
larocquel <leonardolarocque@gmail.com>
2026-06-29 03:10:04 +0100

    C++ 05: ex02 ShrubberyCrationForm WTF

--- CommonCore/Milestone_05/cpp/05/ex02/ShrubberyCreationForm.cpp
+++ CommonCore/Milestone_05/cpp/05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,40 @@
 /*   By: leoaguia <leoaguia@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 22:19:37 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/27 22:19:38 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/06/29 02:59:12 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// ShrubberyCreationForm.cpp
+
+#include "ShrubberyCreationForm.hpp"
+
+// Default Constructor
+ShrubberyCreationForm::ShrubberyCreationForm()
+{
+
+}
+
+// Copy Constructor
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other)
+{
+
+}
+
+// Copy Assignment Operator
+ShrubberyCreationForm&	ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
+{
+
+}
+
+// Destructor
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+
+}
+
+// Virtual Pure Method from Base Class
+void ShrubberyCreationForm::execute(const Bureaucrat& executor) const
+{
+
+}
--- CommonCore/Milestone_05/cpp/05/ex02/ShrubberyCreationForm.hpp
+++ CommonCore/Milestone_05/cpp/05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,33 @@
 /*   By: leoaguia <leoaguia@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 22:19:26 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/27 22:19:27 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/06/29 02:58:50 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// ShrubberyCreationForm.hpp
+
+#ifndef SHRUBBERYCREATIONFORM_HPP
+# define SHRUBBERYCREATIONFORM_HPP
+
+# include "AForm.hpp"
+
+class ShrubberyCreationForm : public AForm
+{
+	private:
+		// Attributes
+		std::string	_target;
+
+	public:
+		// OCF
+		ShrubberyCreationForm();
+		ShrubberyCreationForm(const ShrubberryCreationForm& other);
+		ShrubberyCreationForm&	operator=(const ShrubberryCreationForm& other);
+		~ShrubberyCreationForm();
+
+		// Virtual Pure Method from Base Class
+		void execute(const Bureaucrat& executor) const;
+
+};
+
+#endif
