4b9fc2e92b37d208e36ddc3f729fc35e660e82a2
Leonardo La Rocque <leonardolarocque@gmail.com>
2026-07-01 16:31:17 +0100

    C++ 05: Ex03 Intern

--- CommonCore/Milestone_05/cpp/05/ex03/Intern.hpp
+++ CommonCore/Milestone_05/cpp/05/ex03/Intern.hpp
@@ -6,7 +6,26 @@
 /*   By: leoaguia <leoaguia@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 17:24:59 by leoaguia          #+#    #+#             */
-/*   Updated: 2026/06/30 17:25:00 by leoaguia         ###   ########.fr       */
+/*   Updated: 2026/07/01 16:29:59 by leoaguia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+class AForm;
+
+class Intern
+{
+	private:
+		AForm*	makeShrubbery(const std::string& target) const;
+		AForm*	makeRobotomy(const std::string& taget) const;
+		AForm*	makePresidential(const std::string& target) const;
+
+		
+
+	public:
+
+};
+
+#endif
